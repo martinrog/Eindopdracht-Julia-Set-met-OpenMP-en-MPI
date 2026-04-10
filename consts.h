e3dde17eff53c908956162eb5850b2a1794f6bf1
Martin Rog <martinrog14@gmail.com>
2026-04-10 16:33:49 +0200

    constants back to original for testing

--- consts.h
+++ consts.h
@@ -1,7 +1,7 @@
 #pragma once
 
 constexpr unsigned int MAX_ITER = 81;        // Good default value
-constexpr unsigned int FRAMES = 1200;         // How long / fluent do you want your animation?
+constexpr unsigned int FRAMES = 750;         // How long / fluent do you want your animation?
 constexpr unsigned int CYCLE_FRAMES = 250;   // When does the rotational part repeat?
-constexpr unsigned int WIDTH = 1000;          // Increase WIDTH/HEIGHT for better results
-constexpr unsigned int HEIGHT = 750;         //    or decrease for faster renders.
+constexpr unsigned int WIDTH = 400;          // Increase WIDTH/HEIGHT for better results
+constexpr unsigned int HEIGHT = 300;         //    or decrease for faster renders.
